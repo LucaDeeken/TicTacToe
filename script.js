aa99a3efd1ab125f0d8ded17d582583bfde33782
Luca Deeken <luca.deeken@fuchs-edv.de>
2024-10-22 10:08:08 +0200

    add objects and functions to the script
    add three objects for the Gameboard, Players and Gameflow.
    Gameboard: First, it generates the board in a 2D Array, which uses the cell function behind it. Added a temporary printBoard for orientation. Then I added a function to markFields with either 1 (Player1) or 4(Playe
    r2). Lastly, I added a function to check the board if someone has three in a line, which is calles winConditions.
    Gameflow: Added a function, which keeps track of the rounds left to play (empty fields)

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+function playerObj () {
 
-
-let player = function(){
+  let player = function(){
 
     let playerScore = 0;
-
+  
     return function(){
-
+  
         if (playerScore===1) {
-            playerScore=2;
+            playerScore=4;
         }else{
             playerScore=1;
         }
     return playerScore;
     }
-}();
+  }();
 
-function Gameboard() {
+
+return {player};
+}
+
+
+
+
+
+
+function Gameboard () {
 
     const rows = 3;
     const columns = 3;
@@ -27,6 +36,26 @@ function Gameboard() {
             board[i].push(Cell());
         }
     }
+
+
+    function Cell() {
+      let value = undefined;
+    
+      // Accept a player's token to change the value of the cell
+      const addToken = (player) => {
+        value = player;
+        return value
+      };
+    
+      // How we will retrieve the current value of this cell through closure
+      const getValue = () => value;
+    
+      return {
+        addToken,
+        getValue
+      };
+    }
+
 
     const getBoard = () => board;
     
@@ -41,75 +70,73 @@ function Gameboard() {
         const a = board[rowIndex][columnIndex].getValue();
         console.log(a);
 
-        if ((board[rowIndex][columnIndex].getValue())===0) {
-            board[rowIndex][columnIndex].addToken((player()));
+        if ((board[rowIndex][columnIndex].getValue())===undefined) {
+            board[rowIndex][columnIndex].addToken((playerObjVariable.player()));
             printBoard();
-            endGame();
-            dazu();
+            gameFlowVariable.endGame();
+            winConditions();
         } else {
             alert("Spot already taken!");
         }
-        
       }
 
-    const endGame = function() {
-
-        let roundsLeftToPlay = 9;
-
-        return function () {
-
-            roundsLeftToPlay-=1;
-            console.log(roundsLeftToPlay);
-            if (roundsLeftToPlay===0) {
-                alert("Spiel vorbei!")
-            };
-            return roundsLeftToPlay;
-        }
-
-    }();
+    
 
 
-    function dazu(){
+    function winConditions(){
         const rowSum = board[1].reduce((acc, cell) => acc + cell.getValue(), 0);
         console.log(rowSum);
+        console.log((board[0][2].getValue())+(board[1][1].getValue())+(board[2][0].getValue()));
+        if (
+            (((board[0].reduce((acc, cell) => acc + cell.getValue(), 0)))===3) || (((board[1].reduce((acc, cell) => acc + cell.getValue(), 0)))===3) || (((board[2].reduce((acc, cell) => acc + cell.getValue(), 0)))===3) ||
+            (((board[0][0].getValue())+(board[1][0].getValue())+(board[2][0].getValue()))===3) || (((board[0][1].getValue())+(board[1][1].getValue())+(board[2][1].getValue()))===3) || (((board[0][2].getValue())+(board[1][2].getValue())+(board[2][2].getValue()))===3) ||
+            (((board[0][0].getValue())+(board[1][1].getValue())+(board[2][2].getValue()))===3) ||  (((board[0][2].getValue())+(board[1][1].getValue())+(board[2][0].getValue()))===3)
+          ) { 
+            let a= alert("hi");
+          } else if (
+            (((board[0].reduce((acc, cell) => acc + cell.getValue(), 0)))===12) || (((board[1].reduce((acc, cell) => acc + cell.getValue(), 0)))===12) || (((board[2].reduce((acc, cell) => acc + cell.getValue(), 0)))===12) ||
+            (((board[0][0].getValue())+(board[1][0].getValue())+(board[2][0].getValue()))===12) || (((board[0][1].getValue())+(board[1][1].getValue())+(board[2][1].getValue()))===12) || (((board[0][2].getValue())+(board[1][2].getValue())+(board[2][2].getValue()))===12) ||
+            (((board[0][0].getValue())+(board[1][1].getValue())+(board[2][2].getValue()))===12) ||  (((board[0][2].getValue())+(board[1][1].getValue())+(board[2][0].getValue()))===12)
+          ) {
+            let b= alert("WON12");
+          };
     return rowSum
     }
-
-
-
-    return {printBoard, getBoard, markField, dazu}
-}
-
-function Cell() {
-    let value = 0;
-  
-    // Accept a player's token to change the value of the cell
-    const addToken = (player) => {
-      value = player;
-      return value
-    };
-  
-    // How we will retrieve the current value of this cell through closure
-    const getValue = () => value;
-  
-    return {
-      addToken,
-      getValue
-    };
-  }
-
-Gameboard();
-
-
-const game = {
-
+    return {printBoard, getBoard, markField, winConditions}
 };
 
 
 
+
+
+const gameFlowVariable = gameFlow();
 const gameBoardInstance = Gameboard();
+const playerObjVariable = playerObj();
 const boardState = gameBoardInstance.getBoard(); // Zugriff auf getBoard von außen
 const markField = gameBoardInstance.markField;
 const printBoard = gameBoardInstance.printBoard;
-const winning = gameBoardInstance.dazu();
+const winning = gameBoardInstance.winConditions();
 console.log(boardState);
+
+
+
+function gameFlow () {
+
+  const endGame = function() {
+
+    let roundsLeftToPlay = 9;
+  
+    return function () {
+  
+        roundsLeftToPlay-=1;
+        console.log(roundsLeftToPlay);
+        if (roundsLeftToPlay===0) {
+            alert("Spiel vorbei!")
+        };
+        return roundsLeftToPlay;
+    }
+  }();
+
+  return {endGame}
+
+}
