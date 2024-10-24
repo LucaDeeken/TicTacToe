7da169a23e84deb0301fa41e9d72ebf8017a4228
Luca Deeken <luca.deeken@fuchs-edv.de>
2024-10-24 10:03:08 +0200

    add point mechanism

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
 const tictactoe = document.getElementsByClassName("tictactoe");
+const startGame = document.getElementById("go");
+const Form = document.getElementById("myForm");
+const leftLayout = document.getElementById("myForm");
+const table = document.getElementById("table");
+const caption = document.getElementsByTagName("caption");
+const winnerName= document.getElementById("captP1");
+const tablePlayer1Name = document.getElementById("player1Name");
+const tablePlayer2Name = document.getElementById("player2Name");
+const PointsP1 = document.getElementById("pointsP1");
+const PointsP2 = document.getElementById("pointsP2");
+
+
+Form.addEventListener("submit", (event) => playerObjVariable.handleSubmit(event));
 
 function playerObj () {
 
-  let player = function(){
-
-    let playerScore = 0;
+  let P1 = "";
+  let P2 = "";
   
+  function playerName (name) {
+    this.name = name;
+  }
+
+  let player = function(){
+  
+    let playerScore = 0;
     return function(){
   
         if (playerScore==="X") {
             playerScore="O";
-        }else{
+        } else {
             playerScore="X";
         }
     return playerScore;
     }
   }();
-return {player};
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const namePlayer1Value = document.getElementById("nameP1").value;
+    const namePlayer2Value = document.getElementById("nameP2").value;
+    P1 = new playerName (namePlayer1Value);
+    P2 = new playerName (namePlayer2Value);
+  }
+  
+  function getPlayers() {
+    return { P1, P2 };
+  }
+return {player, handleSubmit, playerName, getPlayers};
 }
 
 function Gameboard () {
@@ -73,42 +104,78 @@ function Gameboard () {
     }
   };
 
+  const score = function() {
+    let scorePlayerA= 0;
+    let scorePlayerB= 0;
+
+    return function () {
+      if(captP1.style.color==="red") {
+        scorePlayerA+=1;
+        PointsP1.textContent=scorePlayerA;
+      } else {
+        scorePlayerB+=1;
+        PointsP2.textContent=scorePlayerB;
+      }
+        return { scorePlayerA, scorePlayerB }
+      }
+  }();
+
   function winConditions(){
-    const rowSum = board[1].reduce((acc, cell) => acc + cell.getValue(), 0);
+    const players = playerObjVariable.getPlayers();
     if (
       (((board[0][0].getValue())+(board[0][1].getValue())+(board[0][2].getValue()))==="XXX") || (((board[1][0].getValue())+(board[1][1].getValue())+(board[1][2].getValue()))==="XXX") || (((board[2][0].getValue())+(board[2][1].getValue())+(board[2][2].getValue()))==="XXX") ||
       (((board[0][0].getValue())+(board[1][0].getValue())+(board[2][0].getValue()))==="XXX") || (((board[0][1].getValue())+(board[1][1].getValue())+(board[2][1].getValue()))==="XXX") || (((board[0][2].getValue())+(board[1][2].getValue())+(board[2][2].getValue()))==="XXX") ||
       (((board[0][0].getValue())+(board[1][1].getValue())+(board[2][2].getValue()))==="XXX") ||  (((board[0][2].getValue())+(board[1][1].getValue())+(board[2][0].getValue()))==="XXX")
         ) { 
-        let a= alert("PlayerOne wins");
+        for (let i=0; i<caption.length;i++)
+          caption[i].classList.remove("hiddenWon");
+        captP1.textContent=players.P1.name;
+        captP1.style.color="red";
+        score();
+        PointsP1.textContent=scorePlayerA;
     } else if (
       (((board[0][0].getValue())+(board[0][1].getValue())+(board[0][2].getValue()))==="OOO") || (((board[1][0].getValue())+(board[1][1].getValue())+(board[1][2].getValue()))==="OOO") || (((board[2][0].getValue())+(board[2][1].getValue())+(board[2][2].getValue()))==="OOO") ||
       (((board[0][0].getValue())+(board[1][0].getValue())+(board[2][0].getValue()))==="OOO") || (((board[0][1].getValue())+(board[1][1].getValue())+(board[2][1].getValue()))==="OOO") || (((board[0][2].getValue())+(board[1][2].getValue())+(board[2][2].getValue()))==="OOO") ||
       (((board[0][0].getValue())+(board[1][1].getValue())+(board[2][2].getValue()))==="OOO") ||  (((board[0][2].getValue())+(board[1][1].getValue())+(board[2][0].getValue()))==="OOO")
         ) {
-        let b= alert("PlayerTwo wins");
+        for (let i=0; i<caption.length;i++)
+          caption[i].classList.remove("hiddenWon");
+        captP1.textContent=players.P2.name;
+        captP1.style.color="blue";
+        score();
         };
   }
 
-  for(let i=0; i<tictactoe.length;i++) {
-    tictactoe[i].addEventListener("click", (event) => {
-      let clickedField = event.target;
-      let rowIndex = clickedField.dataset.row;
-      let columnIndex = clickedField.dataset.column;
-      const markField = (rowIndex, columnIndex) => {
-        if ((board[rowIndex][columnIndex].getValue())===undefined) {
-          board[rowIndex][columnIndex].addToken((playerObjVariable.player()));
-          printBoard();
-          winConditions();
-          gameFlowVariable.endGame();
-        } else {
-          alert("Spot already taken!");
+  startGame.addEventListener("click", () => {
+    playerObjVariable.handleSubmit(event);
+    leftLayout.classList.add("hidden");
+    table.classList.remove("hidden");
+    const players = playerObjVariable.getPlayers();
+    console.log(players);
+    tablePlayer1Name.textContent=players.P1.name;
+    tablePlayer2Name.textContent=players.P2.name;
+    for(let i=0; i<tictactoe.length;i++) {
+      tictactoe[i].classList.remove("hidden");
+      tictactoe[i].addEventListener("click", (event) => {
+        let clickedField = event.target;
+        let rowIndex = clickedField.dataset.row;
+        let columnIndex = clickedField.dataset.column;
+        const markField = (rowIndex, columnIndex) => {
+          if ((board[rowIndex][columnIndex].getValue())===undefined) {
+            board[rowIndex][columnIndex].addToken((playerObjVariable.player()));
+            printBoard();
+            winConditions();
+            gameFlowVariable.endGame();
+          } else {
+            alert("Spot already taken!");
+            }
           }
-        }
-        markField(rowIndex, columnIndex);
-    });
-  }
-  return {printBoard, getBoard, winConditions, reset}
+          markField(rowIndex, columnIndex);
+      });
+    }
+  })
+  
+  return {printBoard, getBoard, winConditions, reset, init, score}
 };
 
 const gameFlowVariable = gameFlow();
